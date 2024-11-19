b830443ab2bef66c620a233e07c8af7bf332a687
Giuliano Simone <giulianosimone9@gmail.com>
2024-11-19 17:51:57 -0300

    PWA ready

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      background_color: '#42B883'
+    },
+    workboxOptions: {
+      // Workbox options
+    }
+  }
 })
