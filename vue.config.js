732ff9fbacdee751a98ea28f1846edf130c477b7
Giuliano Simone <giulianosimone9@gmail.com>
2024-11-19 17:58:37 -0300

    Changed spa config

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
+    name: 'Persnota',
+    themeColor: '#004254',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    manifestOptions: {
-      background_color: '#42B883'
-    },
     workboxOptions: {
       // Workbox options
     }
